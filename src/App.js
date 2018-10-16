b0f80a29cb62fcbcc855ea4aade36d583f779199
ilvecchioRandy <filipporandon.bus@gmail.com>
2018-10-16 11:53:22 +0200

    -s

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      nome: "",
+      json: {},
     }
+    this.update = this.update.bind(this);
   };
 
+  
+
   update(){
+    var j;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         // Typical action to be performed when the document is ready:
-        let json = JSON.parse(xhttp.responseText);
-        document.getElementById("app").innerHTML = json.pokemon_entries[0].pokemon_species.name;
+        j = JSON.parse(xhttp.responseText);
+        document.getElementById("app").innerHTML = j.pokemon_entries[0].pokemon_species.name;
       }
     };
     xhttp.open("GET", "https://pokeapi.co/api/v2/pokedex/1/", true);
     xhttp.send();
+    setTimeout(() =>{
+      this.setState({
+        json: j,
+      })
+      alert(JSON.stringify(this.state.json));
+    }, 6000);
   };
-
   // this.update = this.update.bind(this);
 
   render() {
