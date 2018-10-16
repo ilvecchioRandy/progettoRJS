bc453a778c1d5e97c2138e5333b061b69df9c1da
ilvecchioRandy <filipporandon.bus@gmail.com>
2018-10-16 14:46:53 +0200

    -s

--- src/App.js
+++ src/App.js
@@ -6,50 +6,35 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      json: "",
+      json: {},
     }
-    // this.update = this.update.bind(this);
-    this.fetching = this.fetching.bind(this);
+    
   };
-
-  fetching(){
+  componentWillMount(){
     fetch('https://pokeapi.co/api/v2/pokedex/1/')
-    .then((response) => {
-      return response.json();
-    })
-    .then((myJson) =>{
-      this.setState({
-        json: JSON.stringify(myJson)
-      });
-    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) =>{
+        this.setState({
+          json: myJson      
+        });
+        console.log(this.state.json.pokemon_entries)
+      })
   }
-
-  // update(){
-  //   var j;
-  //   var xhttp = new XMLHttpRequest();
-  //   xhttp.onreadystatechange = function() {
-  //     if (this.readyState === 4 && this.status === 200) {
-  //       // Typical action to be performed when the document is ready:
-  //       j = JSON.parse(xhttp.responseText);
-  //       document.getElementById("app").innerHTML = j.pokemon_entries[0].pokemon_species.name;
-  //     }
-  //   };
-  //   xhttp.open("GET", "https://pokeapi.co/api/v2/pokedex/1/", true);
-  //   xhttp.send();
-  //   setTimeout(() =>{
-  //     this.setState({
-  //       json: j,
-  //     })
-  //     alert(JSON.stringify(this.state.json));
-  //   }, 6000);
-  // };
-  // this.update = this.update.bind(this);
+      
 
   render() {
     return(
-      <div>
-      <p id="1" onClick = {this.fetching}>Ciao</p>
-    </div>
+      <div id="container">
+        <ul>
+          {
+            this.state.json.pokemon_entries.map((pokemon, index) => 
+              <li>{pokemon.name}</li>
+            )
+          }
+        </ul>
+      </div>
     )
   }
 
