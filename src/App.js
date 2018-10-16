f3a5c7d410be2c6f60ec370a7588579d0f566976
jacowarrion <44180644+jacowarrion@users.noreply.github.com>
2018-10-16 16:57:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,67 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      json: {},
+      json: {
+        pokemon_entries: [
+          // {"pokemon_species": {
+          //   "name":"",
+          // }}
+        ]
+      },
+      jsonDescr: []
     }
-    
+
   };
-  componentWillMount(){
+  componentDidMount() {
     fetch('https://pokeapi.co/api/v2/pokedex/1/')
       .then((response) => {
         return response.json();
       })
-      .then((myJson) =>{
+      .then((myJson) => {
         this.setState({
-          json: myJson      
+          json: myJson
         });
-        console.log(this.state.json.pokemon_entries)
       })
   }
-      
+
+  showDescription(index) {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + index + '/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        let x = {
+          "types": myJson.types[0].type.name
+        }
+        if( myJson.types.length === 1){
+          x.types += ' - '+myJson.types[1].type.name
+        }
+        this.setState({
+          jsonDescr: x
+        });
+      })
+  }
 
   render() {
-    return(
+    return (
       <div id="container">
-        <ul>
-          {
-            this.state.json.pokemon_entries.map((pokemon, index) => 
-              <li>{pokemon.name}</li>
-            )
-          }
-        </ul>
+        <div id="sas">
+          <ul>
+            <li> {this.state.jsonDescr.types} </li>
+            <li> </li>
+            <li> </li>
+          </ul>
+        </div>
+      <ul>
+        {
+          this.state.json.pokemon_entries.map((pokemon, index) =>
+            <li key={index + 1} onClick={() => { this.showDescription(index + 1) }}>{pokemon.pokemon_species.name} --- {pokemon.pokemon_species.url}</li>
+          )
+        }
+      </ul>
       </div>
     )
   }
