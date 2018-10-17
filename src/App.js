5b6101bde14c893f71beec2db0acf51d2ca4d4f4
jacopo <jacopoxpippo@gmail.com>
2018-10-17 21:56:22 +0200

    -s

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ class App extends React.Component {
     this.state = {
       json: {
         pokemon_entries: [
+
         ]
       },
       jsonDescr: [],
       jsonFlavor: [],
       jsonSprites: [],
-      currentSelect: ""
+      SpriteName: "",
+      currentSelect: "",
     }
   };
   componentDidMount() {
@@ -24,34 +26,36 @@ class App extends React.Component {
       })
       .then((myJson) => {
         this.setState({
-          json: myJson
+          json: myJson,
         });
       })
   }
 
-  seleziona(index){
-    if(document.getElementById("li" + index).style.backgroundColor === "gold"){
+  seleziona(index) {
+    if (document.getElementById("li" + index).style.backgroundColor === "gold") {
       document.getElementById("li" + index).style.backgroundColor = "gold";
     }
-    else{
-      $("#img").css({display: "none"})
+    else {
+      $("#img").css({ display: "none" })
       document.getElementById("li" + index).style.backgroundColor = "gold";
-    $("#li" + index).css({fontSize: "16pt"});
-    if(this.state.currentSelect !== ""){
-      $('#' + this.state.currentSelect).css({fontSize: "15pt"});
-      document.getElementById(this.state.currentSelect).style.backgroundColor = "";
-    } 
+      $("#li" + index).css({ fontSize: "16pt" });
+      if (this.state.currentSelect !== "") {
+        $('#' + this.state.currentSelect).css({ fontSize: "15pt" });
+        document.getElementById(this.state.currentSelect).style.backgroundColor = "";
+      }
     }
-    
+
     this.setState({
       currentSelect: "li" + index
     })
-    
+
   }
 
   showDescription(index) {
+    console.log(" xx "+this.state.json.pokemon_entries[index].pokemon_species.name)
     this.setState({
-      jsonSprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + index + '.png'
+      jsonSprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + index + '.png',
+      SpriteName: this.state.json.pokemon_entries[index-1].pokemon_species.name
     })
     fetch('https://pokeapi.co/api/v2/pokemon/' + index + '/')
       .then((response) => {
@@ -61,15 +65,7 @@ class App extends React.Component {
         let x = {
           "types": myJson.types[0].type.name
         }
-        if (myJson.types.length === 2) {
-          x.types += ' - ' + myJson.types[1].type.name
-          document.getElementById("type").style.paddingLeft = "47%";
-          document.getElementById("type").style.textAlign = "left";
-        }
-        else{
-          document.getElementById("type").style.paddingLeft = "48.9%";
-          document.getElementById("type").style.textAlign = "left";
-        }
+        if (myJson.types.length === 2) x.types += ' - ' + myJson.types[1].type.name
         this.setState({
           jsonDescr: x
         });
@@ -98,59 +94,57 @@ class App extends React.Component {
         })
       })
   }
-  highlight(index){
-    if(document.getElementById("li" + index).style.backgroundColor !== "gold"){
-      $("#li" + index).css("backgroundColor" , "rgb(253, 90, 31)");
+  highlight(index) {
+    if (document.getElementById("li" + index).style.backgroundColor !== "gold") {
+      $("#li" + index).css("backgroundColor", "rgb(253, 90, 31)");
     }
   }
-  dehighlight(index){
-    if(document.getElementById("li" + index).style.backgroundColor !== "gold"){
-      $("#li" + index).css("backgroundColor" , "coral");
+  dehighlight(index) {
+    if (document.getElementById("li" + index).style.backgroundColor !== "gold") {
+      $("#li" + index).css("backgroundColor", "coral");
     }
   }
-  onChangeText(value){
+  onChangeText(value) {
+    console.log(this.state.json)
     let i;
     let vet = this.state.json.pokemon_entries;
-    if(value.length > 2){
-      for(i = 0; i < vet.length; i++){
-        if(this.state.json.pokemon_entries[i].pokemon_species.name.includes(value)){
-          
-        }
+    for (i = 0; i < vet.length; i++) {
+      if (this.state.json.pokemon_entries[i].pokemon_species.name.includes(value.toLowerCase())) {
+        $("#span" + i).stop(false, true).fadeIn();
+      } else {
+        $("#span" + i).stop(false, true).fadeOut();
       }
     }
-    
-  }
-  search(event){
-    event.preventDefault();
   }
 
   render() {
     return (
       <div id="container">
         <div id="info">
-          <div id="sprite"> <img id="img" src={this.state.jsonSprites} alt="" onLoad={()=>{$("img").fadeIn()}} /> </div>
-            <div id="ty">type:</div>
-            <div id="type"><span>{this.state.jsonDescr.types}</span> </div>
-            <div id="de">description:</div>
-            <div id="description">{this.state.jsonFlavor.flavor}</div>
+          <div id="sprite"><div>{this.state.SpriteName.toUpperCase()}</div> <img id="img" src={this.state.jsonSprites} alt="" onLoad={() => { $("img").fadeIn() }} /> </div>
+          <div id="ty">type:</div>
+          <div id="type"><span>{this.state.jsonDescr.types}</span> </div>
+          <div id="de">description:</div>
+          <div id="description">{this.state.jsonFlavor.flavor}</div>
         </div>
         <div id="lista">
           <ol>
             <hr />
             {
               this.state.json.pokemon_entries.map((pokemon, index) =>
-                <span key={index + 1}><li id = {"li" + index} onMouseOver = {() =>{ this.highlight(index) }} onMouseOut = {() =>{this.dehighlight(index)}} 
-                onClick={() => { this.showDescription(index + 1); this.seleziona(index) }}>
-                <span id="sp">{index + 1}. {pokemon.pokemon_species.name.toUpperCase()}</span></li><hr></hr></span>
+                <span id={"span" + index} key={index + 1}><li id={"li" + index} onMouseOver={() => { this.highlight(index) }} onMouseOut={() => { this.dehighlight(index) }}
+                  onClick={() => { this.showDescription(index + 1); this.seleziona(index) }}>
+                  <span id="sp">{index + 1}.&nbsp;&nbsp;{pokemon.pokemon_species.name.toUpperCase()}</span></li><hr></hr></span>
               )
             }
           </ol>
         </div>
         <div id="header">
-        <img id="pokeball" src={pokeball} alt="pokeball"></img>
-        <div id="pokeD">POK&Eacute;DEX</div>
-        <div id="searchBar"><form id="myForm" onSubmit = {(event) => this.search(event)}>Search a Pok&eacute;mon:
-        <input type="text" id="ricerca" onChange = {(event) => this.onChangeText(event.target.value)}/></form></div>
+          <img id="pokeball" src={pokeball} alt="pokeball"></img>
+          <div id="pokeD">POK&Eacute;DEX</div>
+          <div id="searchBar">Search a Pok&eacute;mon: &nbsp;
+          <input type="text" id="ricerca" onChange={(event) => this.onChangeText(event.target.value)} />
+          </div>
         </div>
       </div>
     )
