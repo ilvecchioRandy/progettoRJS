92f0f90c3e02164a8795d3ab0c53c90d03c87e74
jacopo <jacopoxpippo@gmail.com>
2018-10-18 01:26:13 +0200

    -s

--- src/App.js
+++ src/App.js
@@ -12,11 +12,81 @@ class App extends React.Component {
 
         ]
       },
-      jsonDescr: [],
+      jsonDescr: [
+        {
+          type:{
+            name:"none"
+          }
+        },
+        {
+          type:{
+            name:"none"
+          }
+        }
+      ],
       jsonFlavor: [],
       jsonSprites: [],
       SpriteName: "",
       currentSelect: "",
+      typeArray: {
+        "bug" : {
+          backgroundPosition: "-13.5px -10px",
+        },
+        "dark" : {
+          backgroundPosition: "-110.5px -10px",
+        },
+        "dragon" : {
+          backgroundPosition: "-207.5px -10px",
+        },
+        "electric" : {
+          backgroundPosition: "-13.5px -60px",
+        },
+        "fairy" : {
+          backgroundPosition: "-110.5px -60px",
+        },
+        "fighting" : {
+          backgroundPosition: "-207.5px -60px",
+        },
+        "fire" : {
+          backgroundPosition: "-13.5px -108px",
+        },
+        "flying" : {
+          backgroundPosition: "-110.5px -108px",
+        },
+        "ghost" : {
+          backgroundPosition: "-207px -108px",
+        },
+        "grass" : {
+          backgroundPosition: "-13.5px -159px",
+        },
+        "ground" : {
+          backgroundPosition: "-110.5px -159px",
+        },
+        "ice" : {
+          backgroundPosition: "-207.5px -159px",
+        },
+        "normal" : {
+          backgroundPosition: "-13.5px -209px",
+        },
+        "poison" : {
+          backgroundPosition: "-110.5px -209px",
+        },
+        "bupsychic" : {
+          backgroundPosition: "-207.5px -209px",
+        },
+        "rock" : {
+          backgroundPosition: "-13.5px -259px",
+        },
+        "steel" : {
+          backgroundPosition: "-110.5px -259px",
+        },
+        "water" : {
+          backgroundPosition: "-207.5px -259px",
+        },
+        "none" : {
+          display: "none",
+        },
+      }
     }
   };
   componentDidMount() {
@@ -52,7 +122,6 @@ class App extends React.Component {
   }
 
   showDescription(index) {
-    console.log(" xx "+this.state.json.pokemon_entries[index].pokemon_species.name)
     this.setState({
       jsonSprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + index + '.png',
       SpriteName: this.state.json.pokemon_entries[index-1].pokemon_species.name
@@ -62,13 +131,25 @@ class App extends React.Component {
         return response.json();
       })
       .then((myJson) => {
-        let x = {
-          "types": myJson.types[0].type.name
-        }
-        if (myJson.types.length === 2) x.types += ' - ' + myJson.types[1].type.name
+        let x = 
+          [
+            {
+              type:{
+                name: myJson.types[0].type.name
+              }
+            },
+            {
+              type:{
+                name:"none"
+              }
+            }
+          ]   
+        if (myJson.types.length === 2) x[1].type.name = myJson.types[1].type.name
         this.setState({
-          jsonDescr: x
+          jsonDescr:  x
         });
+        console.log("-----------------")
+        console.log(this.state.jsonDescr);
       })
     fetch('https://pokeapi.co/api/v2/pokemon-species/' + index + '/')
       .then((response) => {
@@ -123,7 +204,9 @@ class App extends React.Component {
         <div id="info">
           <div id="sprite"><div>{this.state.SpriteName.toUpperCase()}</div> <img id="img" src={this.state.jsonSprites} alt="" onLoad={() => { $("img").fadeIn() }} /> </div>
           <div id="ty">type:</div>
-          <div id="type"><span>{this.state.jsonDescr.types}</span> </div>
+          {/* <div id="type"><span>{this.state.jsonDescr.types}</span> </div> */
+        }
+          <div id="type"> <div id="type1" style={this.state.typeArray[this.state.jsonDescr[0].type.name]}></div> <div id="type2" style={this.state.typeArray[this.state.jsonDescr[1].type.name]}></div> </div>
           <div id="de">description:</div>
           <div id="description">{this.state.jsonFlavor.flavor}</div>
         </div>
